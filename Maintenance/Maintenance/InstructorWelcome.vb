285feff70d12c2b3458faf9c42522ed1dc27cb8e
Andrew <arshephe@uark.edu>
2018-03-16 22:28:55 -0500

    FIXED

--- Maintenance/Maintenance/InstructorWelcome.vb
+++ Maintenance/Maintenance/InstructorWelcome.vb
@@ -3,7 +3,14 @@
     Private Sub Label1_Click(sender As Object, e As EventArgs)
 
     End Sub
+    Public Sub New(ByVal fid As Integer)
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        facID = fid
+    End Sub
     Private Sub btnLogOff_Click(sender As Object, e As EventArgs)
         frmLogin.Show()
         Me.Dispose()
