01d614977a0d06dcb486b2428e18db5327e28048
tcd002 <tcd002@WCOB-LAB068>
2018-03-17 13:38:50 -0500

    updates to AddRecord code

--- Maintenance/Maintenance/AddRecord.vb
+++ Maintenance/Maintenance/AddRecord.vb
@@ -44,22 +44,47 @@ Public Class frmAdd
     Private Sub btnContinue_Click(sender As Object, e As EventArgs) Handles btnContinue.Click
         Dim con As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283f1759; Trusted_Connection=yes"}
 
-        Dim command As New SqlCommand("INSERT INTO  WHERE Username = @username AND Password = @password", con)
+        Dim command As New SqlCommand("INSERT StudentID = @StudID,First_Name = @first,Middle_Name = @middle,Last_Name = @last,SSN = @SSN,LocalAddress1 = @LocalAdd1,LocalAddress2 = @LocalAdd2,Local_City = @LocalCity,LocalStateProvince = @LocalStateProvince,LocalZip = @LocalZip,LocalHomePhone = @LocalHomePhone,PermAddress = @PermAdd1,PermAddress2 = @PermAdd2,Perm_City = @PermCity,PermStateProvince = @PermStateProvince,PermZip = @PermZip,PermHomePhone = @PermHomePhone,Email1 = @Email1,Email2 = @Email2,Birthdate = @birthday,Ethnicity = @ethnicity,Gender = @gender,OriginCountry = @origin,Preferred_name = @preferred INTO Student_Header", con)
 
         'need to fix the data types!
-        'command.Parameters.Add("@first", SqlDbType.VarChar).Value = txtFirstName.Text
-        'command.Parameters.Add("@middle", SqlDbType.VarChar).Value = txtMiddleName.Text
-        'command.Parameters.Add("@last", SqlDbType.VarChar).Value = txtLastName.Text
-        'command.Parameters.Add("@preferred", SqlDbType.VarChar).Value = txtPreferred.Text
         'command.Parameters.Add("@StudID", SqlDbType.VarChar).Value = txtStudentID.Text
+        command.Parameters.Add("@first", SqlDbType.VarChar).Value = txtFirstName.Text
+        command.Parameters.Add("@middle", SqlDbType.VarChar).Value = txtMiddleName.Text
+        command.Parameters.Add("@last", SqlDbType.VarChar).Value = txtLastName.Text
         'command.Parameters.Add("@SSN", SqlDbType.VarChar).Value = txtSSN.Text
+        'local address in form = former address OR Mailing address???????
+        'command.Parameters.Add("@LocalAdd1", SqlDbType.VarChar).Value = 
+        'command.Parameters.Add("@LocalAdd2", SqlDbType.VarChar).Value = 
+        'command.Parameters.Add("@LocalCity", SqlDbType.VarChar).Value = 
+
+        'what is the province? a county???
+        'command.Parameters.Add("@LocalStateProvince", SqlDbType.VarChar).Value = 
+
+        'command.Parameters.Add("@LocalZip", SqlDbType.VarChar).Value = 
+        'command.Parameters.Add("@LocalHomePhone", SqlDbType.VarChar).Value = txtCurrentPhone.Text
+        'permanent address in form = perm
+        command.Parameters.Add("@PermAdd1", SqlDbType.VarChar).Value = txtPAddress.Text
+        'command.Parameters.Add("@PermAdd2", SqlDbType.VarChar).Value = 
+        command.Parameters.Add("@PermCity", SqlDbType.VarChar).Value = txtPCity.Text
+
+        'another province...?
+        'command.Parameters.Add("@PermStateProvince", SqlDbType.VarChar).Value = 
+
+        'command.Parameters.Add("@PermZip", SqlDbType.VarChar).Value = txtPZipCode.Text
+        'command.Parameters.Add("@PermHomePhone", SqlDbType.VarChar).Value = txtPPhone.Text
+        command.Parameters.Add("@Email1", SqlDbType.VarChar).Value = txtEmail.Text
+
+        'no second email field..
+        'command.Parameters.Add("@Email2", SqlDbType.VarChar).Value = 
+
         'command.Parameters.Add("@birthday", SqlDbType.VarChar).Value = txtBirthday1.Text
-        'command.Parameters.Add("@origin", SqlDbType.VarChar).Value = cbxOrigin.SelectedText
         'command.Parameters.Add("@ethnicity", SqlDbType.VarChar).Value = cbxEthnic.SelectedText
-        'command.Parameters.Add("@gender", SqlDbType.VarChar).Value = cbxGender.SelectedText
+        command.Parameters.Add("@gender", SqlDbType.VarChar).Value = cbxGender.SelectedText
+        'command.Parameters.Add("@origin", SqlDbType.VarChar).Value = cbxOrigin.SelectedText
+        command.Parameters.Add("@preferred", SqlDbType.VarChar).Value = txtPreferred.Text
+
+        'so where does this info go???
         'command.Parameters.Add("@citizenship", SqlDbType.VarChar).Value = cbxCitizenship.SelectedText
-
-
 
 
     End Sub
