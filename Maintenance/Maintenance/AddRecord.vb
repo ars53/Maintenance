7a8117861fdb11e17f927f22def59c3c771ef65e
tcd002 <tcd002@WCOB-LAB048>
2018-03-21 12:28:48 -0500

    tweaking addrecord more

--- Maintenance/Maintenance/AddRecord.vb
+++ Maintenance/Maintenance/AddRecord.vb
@@ -47,11 +47,12 @@ Public Class frmAdd
         Dim command As New SqlCommand("INSERT StudentID = @StudID,First_Name = @first,Middle_Name = @middle,Last_Name = @last,SSN = @SSN,LocalAddress1 = @LocalAdd1,LocalAddress2 = @LocalAdd2,Local_City = @LocalCity,LocalStateProvince = @LocalStateProvince,LocalZip = @LocalZip,LocalHomePhone = @LocalHomePhone,PermAddress = @PermAdd1,PermAddress2 = @PermAdd2,Perm_City = @PermCity,PermStateProvince = @PermStateProvince,PermZip = @PermZip,PermHomePhone = @PermHomePhone,Email1 = @Email1,Email2 = @Email2,Birthdate = @birthday,Ethnicity = @ethnicity,Gender = @gender,OriginCountry = @origin,Preferred_name = @preferred INTO Student_Header", con)
 
         'need to fix the data types!
-        'command.Parameters.Add("@StudID", SqlDbType.VarChar).Value = txtStudentID.Text
+        command.Parameters.Add("@StudID", SqlDbType.Int).Value = txtStudentID.Text
         command.Parameters.Add("@first", SqlDbType.VarChar).Value = txtFirstName.Text
         command.Parameters.Add("@middle", SqlDbType.VarChar).Value = txtMiddleName.Text
         command.Parameters.Add("@last", SqlDbType.VarChar).Value = txtLastName.Text
-        'command.Parameters.Add("@SSN", SqlDbType.VarChar).Value = txtSSN.Text
+        command.Parameters.Add("@SSN", SqlDbType.VarChar).Value = txtSSN.Text
+
         'local address in form = former address OR Mailing address???????
         'command.Parameters.Add("@LocalAdd1", SqlDbType.VarChar).Value = 
         'command.Parameters.Add("@LocalAdd2", SqlDbType.VarChar).Value = 
@@ -61,7 +62,7 @@ Public Class frmAdd
         'command.Parameters.Add("@LocalStateProvince", SqlDbType.VarChar).Value = 
 
         'command.Parameters.Add("@LocalZip", SqlDbType.VarChar).Value = 
-        'command.Parameters.Add("@LocalHomePhone", SqlDbType.VarChar).Value = txtCurrentPhone.Text
+        command.Parameters.Add("@LocalHomePhone", SqlDbType.Int).Value = txtCurrentPhone.Text
         'permanent address in form = perm
         command.Parameters.Add("@PermAdd1", SqlDbType.VarChar).Value = txtPAddress.Text
         'command.Parameters.Add("@PermAdd2", SqlDbType.VarChar).Value = 
@@ -70,17 +71,17 @@ Public Class frmAdd
         'another province...?
         'command.Parameters.Add("@PermStateProvince", SqlDbType.VarChar).Value = 
 
-        'command.Parameters.Add("@PermZip", SqlDbType.VarChar).Value = txtPZipCode.Text
-        'command.Parameters.Add("@PermHomePhone", SqlDbType.VarChar).Value = txtPPhone.Text
+        command.Parameters.Add("@PermZip", SqlDbType.Int).Value = txtPZipCode.Text
+        command.Parameters.Add("@PermHomePhone", SqlDbType.Int).Value = txtPPhone.Text
         command.Parameters.Add("@Email1", SqlDbType.VarChar).Value = txtEmail.Text
-
         'no second email field originally, has been added to form to fit DB
-        'command.Parameters.Add("@Email2", SqlDbType.VarChar).Value = txtEmail2.Text
+        command.Parameters.Add("@Email2", SqlDbType.VarChar).Value = txtEmail2.Text
 
-        'command.Parameters.Add("@birthday", SqlDbType.VarChar).Value = txtBirthday1.Text
-        'command.Parameters.Add("@ethnicity", SqlDbType.VarChar).Value = cbxEthnic.SelectedText
-        command.Parameters.Add("@gender", SqlDbType.VarChar).Value = cbxGender.SelectedText
-        'command.Parameters.Add("@origin", SqlDbType.VarChar).Value = cbxOrigin.SelectedText
+        command.Parameters.Add("@birthday", SqlDbType.DateTime).Value = txtBirthday1.Text
+        'why is this set to a 1 number int variable?
+        'command.Parameters.Add("@ethnicity", SqlDbType.Int).Value = cbxEthnic.SelectedText
+        command.Parameters.Add("@gender", SqlDbType.Char).Value = cbxGender.SelectedText
+        command.Parameters.Add("@origin", SqlDbType.VarChar).Value = cbxOrigin.SelectedText
         command.Parameters.Add("@preferred", SqlDbType.VarChar).Value = txtPreferred.Text
 
         'so where does this info go???
@@ -110,18 +111,19 @@ Public Class frmAdd
     Private Sub btnConfirm3_Click(sender As Object, e As EventArgs) Handles btnConfirm3.Click
         Dim con As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283f1759; Trusted_Connection=yes"}
         'below connection needs proper table name to insert into
-        'Dim command As New SqlCommand("INSERT  INTO Student_", con)
+        'Dim command As New SqlCommand("INSERT DateApplied = @dateapplied, DateAccepted = @dateaccepted, DateEnrolled = @dateenrolled, DateDeclined = @datedeclined, LOR1 = @, LOR2, = @, LOR3 = @, TranscriptsTN = @, ResumeYN = @, AssistantShip = @, GMATVerbal, = @GMATverbal, GMATQuant = @GMATquant, GMATTotal = @GMATtotal, GMATPercent = @, GRE = @, TOEFL = @, UndergradIns = @, UndergradGPA = @, UndergradMa = @, Degree = @, DegreeDate = @, SecondDegree = @,SecondDegree = @, SecondDegree = @, YearsWorkEx = @ @, DegreeSeeki = @, StatusFTPT = @, NondegreeS = @, StudentID = @  INTO StudentApp", con)
 
         'need to fix the data types!
         'need name of coulms in table to data entry
-        'command.Parameters.Add("@GMATdate", SqlDbType.VarChar).Value = txtGMATDate.Text
-        'command.Parameters.Add("@TOEFLdate", SqlDbType.VarChar).Value = txtTOEFLDate.Text
-        'command.Parameters.Add("@TSEdate", SqlDbType.VarChar).Value = txtTSEDate.Text
-        'command.Parameters.Add("@GMATverbal", SqlDbType.VarChar).Value = txtGMATVerbal.Text
-        'command.Parameters.Add("@GMATquant", SqlDbType.VarChar).Value = txtGMATQuant.Text
-        'command.Parameters.Add("@GMATtotal", SqlDbType.VarChar).Value = txtGMATTotal.Text
-        'command.Parameters.Add("@underGPA", SqlDbType.VarChar).Value = txtUnderGPA.Text
-        'command.Parameters.Add("@GPAsenior", SqlDbType.VarChar).Value = txtGPASenior.Text
+        'command.Parameters.Add("@GMATdate", SqlDbType.DateTime).Value = txtGMATDate.Text
+        'command.Parameters.Add("@TOEFLdate", SqlDbType.DateTime).Value = txtTOEFLDate.Text
+        'command.Parameters.Add("@TSEdate", SqlDbType.DateTime).Value = txtTSEDate.Text
+
+        'command.Parameters.Add("@GMATverbal", SqlDbType.Int).Value = txtGMATVerbal.Text
+        'command.Parameters.Add("@GMATquant", SqlDbType.Int).Value = txtGMATQuant.Text
+        'command.Parameters.Add("@GMATtotal", SqlDbType.Int).Value = txtGMATTotal.Text
+        'command.Parameters.Add("@underGPA", SqlDbType.Decimal).Value = txtUnderGPA.Text
+        'command.Parameters.Add("@GPAsenior", SqlDbType.Decimal).Value = txtGPASenior.Text
 
         'command.Parameters.Add("@cname1", SqlDbType.VarChar).Value = txtCName1.Text
         'command.Parameters.Add("@CName2", SqlDbType.VarChar).Value = txtCName2.Text
@@ -132,8 +134,8 @@ Public Class frmAdd
         'command.Parameters.Add("@CFrom1", SqlDbType.VarChar).Value = txtCFrom1.Text
         'command.Parameters.Add("@CFrom2", SqlDbType.VarChar).Value = txtCFrom2.Text
 
-        'command.Parameters.Add("@CGPA1", SqlDbType.VarChar).Value = txtCGPA1.Text
-        'command.Parameters.Add("@CGPA2", SqlDbType.VarChar).Value = txtCGPA2.Text
+        'command.Parameters.Add("@CGPA1", SqlDbType.Decimal).Value = txtCGPA1.Text
+        'command.Parameters.Add("@CGPA2", SqlDbType.Decimal).Value = txtCGPA2.Text
 
         'command.Parameters.Add("@HoursEarned1", SqlDbType.VarChar).Value = txtHoursEarned1.Text
         'command.Parameters.Add("@HoursEarned2", SqlDbType.VarChar).Value = txtHoursEarned2.Text
@@ -161,17 +163,19 @@ Public Class frmAdd
     Private Sub btnConfirm4_Click(sender As Object, e As EventArgs) Handles btnConfirm4.Click
         Dim con As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283f1759; Trusted_Connection=yes"}
         'below connection needs proper table name to insert into
-        'Dim command As New SqlCommand("INSERT  INTO Student_", con)
+        'Dim command As New SqlCommand("INSERT  INTO StudentApp", con)
 
         'need to fix the data types!
         'need name of coulms in table to data entry
         'command.Parameters.Add("@Status", SqlDbType.VarChar).Value = txtStatus.Text
+
         'command.Parameters.Add("@DateApplied", SqlDbType.VarChar).Value = txtDateApplied.Text
         'command.Parameters.Add("@AStatus", SqlDbType.VarChar).Value = txtAStatus.Text
         'command.Parameters.Add("@FeePaid", SqlDbType.VarChar).Value = txtFeePaid.Text
 
         'command.Parameters.Add("@SEnrolled", SqlDbType.VarChar).Value = txtSEnrolled.Text
         'command.Parameters.Add("@DEnrolled", SqlDbType.VarChar).Value = txtDEnrolled.Text
+
 
         'command.Parameters.Add("@Declined", SqlDbType.VarChar).Value = txtDeclined.Text
         'command.Parameters.Add("@DDate", SqlDbType.VarChar).Value = txtDDate.Text
